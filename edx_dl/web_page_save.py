3f5b07b1c5054af535fedabd3494dae583c8e645
Joel Turner <jturner421@gmail.com>
2020-02-02 12:55:46 -0600

    wip- added pycopy

--- edx_dl/web_page_save.py
+++ edx_dl/web_page_save.py
@@ -1,7 +1,49 @@
+import logging
 from dataclasses import dataclass
 
 from bs4 import BeautifulSoup
 
+from edx_dl.edx_dl import EDX_HOMEPAGE
+
+
+def pyweb_login(url, headers, username, password, session):
+    """
+    Log in user into the openedx website for pywebcopy.
+    """
+    payload = {'email': username,
+               'password': password
+               }
+
+    response = session.post(url, data=payload, headers=headers)
+    return response
+
+def pywebcopy_get_headers(session, username, password):
+    """
+    Build the Open edX headers to create future requests.
+
+    """
+    logging.info('Building Pywebcopy headers for future requests.')
+
+    headers = {'Accept': 'application/json, text/javascript, */*; q=0.01',
+               'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko)'
+                             'Chrome/79.0.3945.88 Safari/537.36'}
+    session.get(EDX_HOMEPAGE, headers=headers)
+
+    csrftoken = session.cookies._cookies['courses.edx.org']['/']['csrftoken']
+    headers['cookie'] = '; '.join([x.name + '=' + x.value for x in session.cookies])
+
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/79.0.3945.88 Safari/537.36',
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'X-CSRFToken': csrftoken.value,
+        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+        'cookie': headers['cookie'],
+        'Referer': EDX_HOMEPAGE,
+        'X-Requested-With': 'XMLHttpRequest'
+    }
+    logging.debug('PywebCopy Headers built: %s', headers)
+    return headers
 
 
 def extract_course_hierachy(sub_sec,section_list):
