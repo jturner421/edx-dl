17a0db3eb5ffe61d2aa0211beef9bca5cb93246f
Joel Turner <jturner421@gmail.com>
2020-02-11 18:49:19 -0600

    wip - cleaned up key error

--- edx_dl/web_page_save.py
+++ edx_dl/web_page_save.py
@@ -1,14 +1,10 @@
 import logging
-from dataclasses import dataclass
-from operator import itemgetter
+import os
+import pathlib
 
 import pywebcopy
-from pywebcopy import save_webpage
 from bs4 import BeautifulSoup
-import lxml
-import argparse
-import pathlib
-import os
+from pywebcopy import save_webpage
 
 
 def pyweb_login(url, headers, username, password, session):
@@ -21,6 +17,7 @@ def pyweb_login(url, headers, username, password, session):
 
     response = session.post(url, data=payload, headers=headers)
     return response
+
 
 def pywebcopy_get_headers(session, username, password, login_url):
     """
@@ -44,11 +41,12 @@ def pywebcopy_get_headers(session, username, password, login_url):
         'X-CSRFToken': csrftoken.value,
         'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
         'cookie': headers['cookie'],
-        'Referer':login_url,
+        'Referer': login_url,
         'X-Requested-With': 'XMLHttpRequest'
     }
     logging.debug('PywebCopy Headers built: %s', headers)
     return headers
+
 
 def save_web_page(url, target_dir, pyweb_session):
     kwargs = {'zip_project_folder': False,
@@ -58,27 +56,28 @@ def save_web_page(url, target_dir, pyweb_session):
               }
     save_webpage(**kwargs)
 
-def extract_course_hierachy(sub_sec,section_list):
+
+def extract_course_hierachy(sub_sec, section_list):
     sub_sec_list = []
     for child in sub_sec:
         if child is not None:
 
             section_name = child.parent.parent.h3.text.strip()
-            section_name= section_name.replace(":", "").replace("-", "").replace(" ", "_")
+            section_name = section_name.replace(":", "").replace("-", "").replace(" ", "_")
             subsection_heading = child.h4.text.strip()
             links = child.find_all('a')
             link_list = []
             for link in links:
                 link_name = link.text.strip()
                 if link_name.find("\n"):
-                    link_name = link_name[:link_name.find("\n")]
+                    link_name = _strip_characters_from_link_text(link_name)
                 try:
                     link_url = link.attrs['href']
                 except TypeError:
                     continue
                 l = {link_name: link_url}
                 link_list.append(l)
-            sub_section_links = {'subsection': subsection_heading, 'links':link_list}
+            sub_section_links = {'subsection': subsection_heading, 'links': link_list}
             sub_sec_list.append(sub_section_links)
         else:
             pass
@@ -86,9 +85,43 @@ def extract_course_hierachy(sub_sec,section_list):
         section_list.append(section_dict)
     return section_list, sub_sec_list
 
+
+def _strip_characters_from_link_text(link_name):
+    if "\n" in link_name:
+        link_name = link_name[:link_name.find("\n")]
+
+    #link_name = link_name.strip('\n')
+
+    return link_name
+
+
+def set_page_save_path(file_list, output_path, url_key):
+    save_path = next(section_name for section_name in file_list if section_name in section_name)
+    page_save_path = pathlib.Path(output_path).joinpath(save_path, url_key)
+    return page_save_path
+
+
+def create_directory_based_on_downloaded_videos(output_path):
+    file_list = []
+    for entry in os.scandir(output_path):
+        if entry.is_dir():
+            file_list.append(entry.name)
+    return file_list
+
+
+def create_url_dictionary(course_urls):
+    url_dict = dict((i['subsection'], i['links']) for i in course_urls[1])
+    return url_dict
+
+
+def find_subsections(soup):
+    sub_sec = soup.find_all(True, {"class": ["subsection accordion", "subsection accordion graded scored"]})
+    return sub_sec
+
+
 def main():
     username = 'jturner421@gmail.com'
-    password =  'durin7456'
+    password = 'durin7456'
     LOGIN_API = 'https://courses.edx.org/login_ajax'
     course_url = 'https://courses.edx.org/courses/course-v1:Harvardx+HLS2X+1T2020/course/'
     # Prepare PyWebCopy Headers
@@ -100,58 +133,32 @@ def main():
     # Login
     section_names_list = []
     page = pywebcopy.SESSION.get(course_url)
-    soup = BeautifulSoup (page.text, 'lxml')
-    course_name = soup.find(class_= 'page-header-main').h2.text.strip()
+    soup = BeautifulSoup(page.text, 'lxml')
+    course_name = soup.find(class_='page-header-main').h2.text.strip()
     course_name = course_name.replace(":", "").replace("-", "").replace(" ", "_")
     output_path = pathlib.Path(base_output_path).joinpath(course_name)
 
-    sub_sec = soup.find_all(True, {"class":["subsection accordion", "subsection accordion graded scored"]})
+    sub_sec = find_subsections(soup)
     section_list = []
     course_urls = extract_course_hierachy(sub_sec, section_list)
     # create master dictionary of urls for loookup
     # url_dict = dict((i['subsection'], i['links']) for i in course_urls[1])
-    url_dict = dict((i['subsection'], i['links']) for i in course_urls[1])
+    url_dict = create_url_dictionary(course_urls)
     # section_list_dict = dict((i['section_name'], i['subsections']) for i in course_urls[0])
     # get output directories for course
-    file_list = []
-    for entry in os.scandir(output_path):
-        if entry.is_dir():
-            file_list.append(entry.name)
+    file_list = create_directory_based_on_downloaded_videos(output_path)
     # get key
     for sub_section in course_urls[0]:
         section_name = sub_section['section_name']
         url_key = sub_section['subsection']
         # get course list
         urls = url_dict[url_key]
-        save_path = next(section_name for section_name in file_list if section_name in section_name)
-        page_save_path = pathlib.Path(output_path).joinpath(save_path, url_key)
+        page_save_path = set_page_save_path(file_list, output_path, url_key)
         url = urls[0][url_key]
         save_web_page(url, str(page_save_path), pyweb_session)
-        # for index, url in enumerate(urls):
-        #     while index < 1:
-        #         for value in enumerate(url):
-        #             k = (value[1])
-        #             url = url[k]
-        #             save_web_page(url, str(page_save_path), pyweb_session)
-        #             print('done')
-
-
 
         print(section_name)
 
-
-    # for index, value in enumerate(course_urls[0]):
-    #     url_dict_pos  = (index,value)
-    #     for sub_section_key in enumerate(value):
-    #         key = sub_section_key[1]
-    #         sub_section_dict = value[key]
-    #         for i, link_key in enumerate(sub_section_dict):
-    #             for get_link_key in enumerate(link_key):
-    #                 lkey = get_link_key[1]
-    #                 url_dict = sub_section_dict[0][lkey]
-    #                 for url in enumerate(url_dict):
-    #                     ukey = url[1]
-    #                     url_list = url_dict[0][ukey]
 
 if __name__ == '__main__':
     main()
